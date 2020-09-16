1f5b8423feefcd5b8f4d6ac6531f718a2ff23624
AHFXAdam <adam@ahfx.net>
2020-09-15 22:56:52 -0600

    Added new index page

--- scripts/page.js
+++ scripts/page.js
@@ -1,7 +1,7 @@
-window.addEventListener('load', (event) => {
+window.addEventListener('load', (event)=>{
     const lu = document.querySelector('#lastupdated');
     lu.textContent = document.lastModified;
 
-    const cry = document.querySelector('#copyrightyear');
-    cry.textContent = new Date().getFullYear(); 
-  });
+    const cry = document.querySelector("#copyrightyear");
+    cry.textContent = new Date().getFullYear();
+})
